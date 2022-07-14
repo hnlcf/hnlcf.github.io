92972e9dbb35ce4a8faa93d6210747243d5b9975
Changfeng <louchangfeng@outlook.com>
2022-07-14 19:54:13 +0800

    fix: add include-markdown plugin

--- scripts/install-mkdocs.sh
+++ scripts/install-mkdocs.sh
@@ -4,6 +4,7 @@ pip install \
     mkdocs-material \
     mkdocs-macros-plugin \
     mkdocs-literate-nav \
+    mkdocs-include-markdown-plugin \
     mkdocs-git-authors-plugin \
     mkdocs-git-revision-date-localized-plugin \
     mkdocs-git-committers-plugin-2 \
